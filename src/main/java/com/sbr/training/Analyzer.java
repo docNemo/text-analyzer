86f842fb2bc4e22e2c18af80294f95f0f0238050
Урубков Владислав Станиславович <19826347@sberbank.ru>
2022-01-27 14:35:25 +0300

    Added removing extra spaces and redesigned method getReadyString

--- src/main/java/com/sbr/training/Analyzer.java
+++ src/main/java/com/sbr/training/Analyzer.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Analyzer {
     private final static String INDENT = "    ";
+    private final static String SPACE = " ";
     private final static String SEMICOLON = ";";
     private final static String OPENING_BRACE = "{";
     private final static String CLOSING_BRACE = "}";
@@ -12,11 +13,12 @@ public class Analyzer {
     public List<String> analyse(List<String> text) {
         ArrayList<String> cleanText = new ArrayList<>();
         int nestingLevel = 0;
+
         for (String rawStr : text) {
-            List<String> readyString = getString(rawStr);
+            List<String> readyString = getReadyString(rawStr);
             for (String str : readyString) {
 
-                if (str.endsWith(CLOSING_BRACE)) {
+                if (nestingLevel > 0 && str.endsWith(CLOSING_BRACE)) {
                     nestingLevel--;
                 }
 
@@ -24,10 +26,11 @@ public class Analyzer {
                         && !cleanText.get(cleanText.size() - 1).endsWith(SEMICOLON)
                         && !cleanText.get(cleanText.size() - 1).endsWith(OPENING_BRACE)
                         && !cleanText.get(cleanText.size() - 1).endsWith(CLOSING_BRACE)) {
-                    cleanText.set(cleanText.size() - 1, cleanText.get(cleanText.size() - 1) + " " + str);
+                    cleanText.set(cleanText.size() - 1, cleanText.get(cleanText.size() - 1) + SPACE + str);
                 } else {
                     cleanText.add(getIndent(nestingLevel) + str);
                 }
+
                 if (str.endsWith(OPENING_BRACE)) {
                     nestingLevel++;
                 }
@@ -37,27 +40,41 @@ public class Analyzer {
         return cleanText;
     }
 
-    private List<String> getString(String inputString) {
-        inputString = inputString.trim();
+    private List<String> getReadyString(String rawString) {
+        rawString = rawString.trim();
+        ArrayList<String> readyStrings = new ArrayList<>();
         int indexLastSplit = 0;
-        ArrayList<String> finishedString = new ArrayList<>();
-        for (int i = 0; i < inputString.length(); i++) {
-            if (inputString.charAt(i) == SEMICOLON.charAt(0) || inputString.charAt(i) == OPENING_BRACE.charAt(0)) {
-                finishedString.add(inputString.substring(indexLastSplit, i + 1).trim());
-                indexLastSplit = i + 1;
-            } else if (inputString.charAt(i) == CLOSING_BRACE.charAt(0)) {
-                String subStr = inputString.substring(indexLastSplit, i).trim();
-                if (!subStr.isEmpty()) {
-                    finishedString.add(subStr);
+        for (int i = 0; i < rawString.length(); i++) {
+            switch (rawString.charAt(i)) {
+                case ';': {
+                    readyStrings.add(rawString.substring(indexLastSplit, i).trim() + SEMICOLON);
+                    rawString = rawString.substring(i + 1).trim();
+                    i = -1;
+                    break;
                 }
-                finishedString.add(CLOSING_BRACE);
-                indexLastSplit = i + 1;
+                case '{': {
+                    String baseStr = rawString.substring(indexLastSplit, i).trim().equals("") ? "" : rawString.substring(indexLastSplit, i).trim() + SPACE;
+                    readyStrings.add(baseStr + OPENING_BRACE);
+                    rawString = rawString.substring(i + 1).trim();
+                    i = -1;
+                    break;
+                }
+                case '}': {
+                    readyStrings.add(CLOSING_BRACE);
+                    rawString = rawString.substring(i + 1).trim();
+                    i = -1;
+                    break;
+                }
+                case ' ': {
+                    readyStrings.add(rawString.substring(indexLastSplit, i));
+                    rawString = rawString.substring(i + 1).trim();
+                    i = -1;
+                    break;
+                }
             }
+
         }
-        if (indexLastSplit < inputString.length()) {
-            finishedString.add(inputString.substring(indexLastSplit).trim());
-        }
-        return finishedString;
+        return readyStrings;
     }
 
     private String getIndent(int nestingLevel) {
--- src/program.java
+++ src/program.java
@@ -1,9 +1,10 @@
-if (blabla == null){ return 1;}
-        if (blabla == null) {
-        return 1;
-        if (...) {
-        if (...) {
-
+public UserLoginResponse loginUser(UserLoginRequest request) throws BadRequestException {
+        User user = userService.getUserByEmail(request.getEmail());
+        if (user == null) {
+        throw new BadRequestException(ErrorCode.INCORRECT_LOGIN, "login");
         }
-        }
+        String token = tokenService.getToken(user);
+        response.addHeader(SET_AUTH_HEADER_STRING, token);
+        UserLoginResponse loginDtoResponse = new UserLoginResponse(token);
+        return loginDtoResponse;
         }
